e9fdeb57e2e7a6da354c8f8dfa2e27e76a33c493
Appealz <dladnwls121@gmail.com>
2025-06-19 14:40:04 +0900

    data 테이블 연동

---
+++ Assets/LWJ/Data/TableData.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset(AssetPath = "Resources")]
+public class TableData : ScriptableObject
+{
+	public List<WeaponData_Entity> WeaponData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<ArmorData_Entity> ArmorData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<Healkit_Entity> HealkitData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<UnitBaseStats_Entity> UnitBaseStatsData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<MonsterStats_Entity> MonsterStatsData; // Replace 'EntityType' to an actual type that is serializable.
+	//public List<ClassStatsData_Entity> ClassStatsData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<PerkData_Entity> PerkData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<AchievementsData_Entity> AchievementsData; // Replace 'EntityType' to an actual type that is serializable.
+	public List<LevelEXPData_Entity> LevelEXPData; // Replace 'EntityType' to an actual type that is serializable.
+}
+
