6bc9fe1390460aa49cd6f803aa027be29e9272d8
skillwalker <limitstny@naver.com>
2025-06-27 14:33:52 +0900

    애니메이션 해쉬값 클래스

---
+++ Assets/BJJ/01Scripts/Utility/AnimHash.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimHash
+{
+    private static Dictionary<string, int> animHashMap = new Dictionary<string, int>()
+    {
+        {"IsMove" , Animator.StringToHash("IsMove") },
+        {"OnUse", Animator.StringToHash("OnUse") },
+        {"OnReload", Animator.StringToHash("OnReload") },
+        { "OnSkill", Animator.StringToHash("OnSkill")},
+        {"OnAttack", Animator.StringToHash("OnAttack") },
+    };
+
+    public static int GetAnimHash(string type)
+    {
+        if(animHashMap.TryGetValue(type, out var hash))
+            return hash;
+
+        Debug.Log($"AnimHash.cs - GetAnimHash() - {type} is Non AnimParam");
+        return -1;
+    }
+}
